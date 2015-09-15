00e60e218badf09920f14c85798e1847f167a549
Sebastian Celejewski <sebastian.celejewski@wp.pl>
2015-09-15 13:03:52 +0200

    Using HTTP connection instead of an SMS

--- src/main/java/pl/sebcel/gpstracker/plugins/smsnotifier/SMSNotifier.java
+++ src/main/java/pl/sebcel/gpstracker/plugins/smsnotifier/SMSNotifier.java
@@ -1,5 +1,10 @@
 package pl.sebcel.gpstracker.plugins.smsnotifier;
 
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import javax.microedition.io.Connector;
+import javax.microedition.io.HttpConnection;
 import javax.microedition.location.Location;
 import javax.microedition.location.LocationListener;
 import javax.microedition.location.LocationProvider;
@@ -43,10 +48,38 @@ public class SMSNotifier implements AppStateChangeListener, UserActionListener, 
         String message = waypointManager.getWaypointInfo(coordinates);
         if (message != null && message.trim().length() > 0) {
             log.debug("[SMS Notifier] Message: " + message);
+
+            try {
+                String url = "http://www.sebastian-celejewski.pl/Klodziomondo/update.php?message=" + message;
+                HttpConnection httpConnection = (HttpConnection) Connector.open(url);
+                httpConnection.setRequestMethod(HttpConnection.GET);
+                httpConnection.setRequestProperty("User-Agent", "Klodziomondo");
+
+                log.debug("[SMS Notifier] HttpConnection: " + httpConnection);
+
+                int responseCode = httpConnection.getResponseCode();
+                log.debug("[SMS Notifier] Response code: " + responseCode);
+
+                if (responseCode == httpConnection.HTTP_OK) {
+                    StringBuffer sb = new StringBuffer();
+                    OutputStream os = httpConnection.openOutputStream();
+                    InputStream is = httpConnection.openDataInputStream();
+                    int chr;
+                    while ((chr = is.read()) != -1) {
+                        sb.append((char) chr);
+                    }
+
+                    // Web Server just returns the birthday in mm/dd/yy format.
+                    log.debug("[SMS Notifier] Response: " + sb.toString());
+                }
+            }
+
+            catch (Exception ex) {
+                log.debug("[SMS Notifier] Error: " + ex);
+            }
         }
     }
 
     public void providerStateChanged(LocationProvider provider, int newState) {
     }
-
 }
