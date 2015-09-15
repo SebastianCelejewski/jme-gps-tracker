df16de6a7eb66cc9362d558e6996f0066b7b418a
Sebastian Celejewski <sebastian.celejewski@wp.pl>
2015-09-15 13:13:17 +0200

    Refactoring of sending message

--- src/main/java/pl/sebcel/gpstracker/plugins/smsnotifier/SMSNotifier.java
+++ src/main/java/pl/sebcel/gpstracker/plugins/smsnotifier/SMSNotifier.java
@@ -1,7 +1,6 @@
 package pl.sebcel.gpstracker.plugins.smsnotifier;
 
 import java.io.InputStream;
-import java.io.OutputStream;
 
 import javax.microedition.io.Connector;
 import javax.microedition.io.HttpConnection;
@@ -13,7 +12,6 @@ import javax.microedition.location.QualifiedCoordinates;
 import pl.sebcel.gpstracker.events.AppStateChangeListener;
 import pl.sebcel.gpstracker.events.UserActionListener;
 import pl.sebcel.gpstracker.state.AppState;
-import pl.sebcel.gpstracker.state.GpsStatus;
 import pl.sebcel.gpstracker.utils.Logger;
 import pl.sebcel.gpstracker.workflow.AppWorkflow;
 import pl.sebcel.gpstracker.workflow.StatusTransition;
@@ -32,15 +30,16 @@ public class SMSNotifier implements AppStateChangeListener, UserActionListener, 
     public void userSwitchedTo(StatusTransition statusTransition) {
         if (statusTransition.equals(AppWorkflow.START)) {
             log.debug("[SMS Notifier] Starting recording new track.");
+            sendMessage("Start");
         }
         if (statusTransition.equals(AppWorkflow.STOP)) {
             log.debug("[SMS Notifier] Stopped recording track.");
+            sendMessage("Stop");
         }
 
     }
 
     public void appStateChanged(AppState appState) {
-        GpsStatus gpsStatus = appState.getGpsStatus();
     }
 
     public void locationUpdated(LocationProvider provider, Location location) {
@@ -48,35 +47,39 @@ public class SMSNotifier implements AppStateChangeListener, UserActionListener, 
         String message = waypointManager.getWaypointInfo(coordinates);
         if (message != null && message.trim().length() > 0) {
             log.debug("[SMS Notifier] Message: " + message);
+            sendMessage(message);
+        }
+    }
 
-            try {
-                String url = "http://www.sebastian-celejewski.pl/Klodziomondo/update.php?message=" + message;
-                HttpConnection httpConnection = (HttpConnection) Connector.open(url);
-                httpConnection.setRequestMethod(HttpConnection.GET);
-                httpConnection.setRequestProperty("User-Agent", "Klodziomondo");
+    private void sendMessage(String message) {
+        try {
+            String url = "http://www.sebastian-celejewski.pl/Klodziomondo/update.php?message=" + message;
+            HttpConnection httpConnection = (HttpConnection) Connector.open(url);
+            httpConnection.setRequestMethod(HttpConnection.GET);
+            httpConnection.setRequestProperty("User-Agent", "Klodziomondo");
 
-                log.debug("[SMS Notifier] HttpConnection: " + httpConnection);
+            log.debug("[SMS Notifier] HttpConnection: " + httpConnection);
 
-                int responseCode = httpConnection.getResponseCode();
-                log.debug("[SMS Notifier] Response code: " + responseCode);
+            int responseCode = httpConnection.getResponseCode();
+            log.debug("[SMS Notifier] Response code: " + responseCode);
 
-                if (responseCode == httpConnection.HTTP_OK) {
-                    StringBuffer sb = new StringBuffer();
-                    OutputStream os = httpConnection.openOutputStream();
-                    InputStream is = httpConnection.openDataInputStream();
-                    int chr;
-                    while ((chr = is.read()) != -1) {
-                        sb.append((char) chr);
-                    }
-
-                    // Web Server just returns the birthday in mm/dd/yy format.
-                    log.debug("[SMS Notifier] Response: " + sb.toString());
+            if (responseCode == HttpConnection.HTTP_OK) {
+                StringBuffer sb = new StringBuffer();
+                InputStream is = httpConnection.openDataInputStream();
+                int chr;
+                while ((chr = is.read()) != -1) {
+                    sb.append((char) chr);
                 }
-            }
 
-            catch (Exception ex) {
-                log.debug("[SMS Notifier] Error: " + ex);
+                // Web Server just returns the birthday in mm/dd/yy format.
+                log.debug("[SMS Notifier] Response: " + sb.toString());
             }
+            
+            httpConnection.close();
+        }
+
+        catch (Exception ex) {
+            log.debug("[SMS Notifier] Error: " + ex);
         }
     }
 
